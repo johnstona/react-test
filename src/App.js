5c68de3b0dd08dbc43e7ddb841375812b00acc6d
Wan Yau <wan.yau@redmatter.com>
2020-06-30 10:23:40 +0100

    update react

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        List here
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      List here
+    </div>
+  );
 }
 
 export default App;
